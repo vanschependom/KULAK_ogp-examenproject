8876b047a536ba9e3e4d902f0e0878e8407cc271
Vincent Van Schependom <vincent.van.schependom@gmail.com>
2024-04-25 17:58:42 +0200

    Begonnen aan specificatie Temperature, nog niet afgewerkt

--- src/rpg/Temperature.java
+++ src/rpg/Temperature.java
@@ -1,26 +1,90 @@
 package rpg;
+import be.kuleuven.cs.som.annotate.*;
 
+/**
+ * A class representing the temperature.
+ *
+ * @invar 	The temperature must always be valid.
+ * 			| isValidTemperature(getHotness(), getColdness())
+ *
+ * @note	We use TOTAL PROGRAMMING for this class.
+ *
+ * @author	Vincent Van Schependom
+ * @author 	Arne Claerhout
+ * @author 	Flor De Meulemeester
+ * @version 1.0
+ *
+ */
 public class Temperature {
 
+	/**
+	 * A variable referencing the upperbound for both the hotness
+	 * and the coldness; this will not change during the program.
+	 */
 	private static long UPPERBOUND = 10000;
 
-	private long hotness;
-	private long coldness;
+	/**********************************************************
+	 * Constructors
+	 **********************************************************/
 
 	public Temperature(long hotness, long coldness) {
-		if (hotness < 0 || coldness < 0) {
-			throw new IllegalArgumentException("rpg.Temperature cannot be negative");
+		if (isValidTemperature(hotness, coldness)) {
+			// set to the provided temperature
+			setHotness(hotness);
+			setColdness(coldness);
+		} else {
+			// set to the default temperature
+			setHotness(20);
+			setColdness(0);
 		}
-		if (hotness > UPPERBOUND || coldness > UPPERBOUND) {
-			throw new IllegalArgumentException("rpg.Temperature cannot be higher than " + UPPERBOUND);
-		}
-		if (hotness != 0 && coldness != 0) {
-			throw new IllegalArgumentException("rpg.Temperature cannot be both hot and cold");
-		}
-		this.hotness = hotness;
-		this.coldness = coldness;
 	}
 
+	/**
+	 * A variable referencing the hotness of the temperature.
+	 */
+	private long hotness;
+
+	/**
+	 * A getter for the hotness of the temperature.
+	 */
+	@Basic
+	public long getHotness() {
+		return hotness;
+	}
+
+	/**
+	 * A setter for the hotness of the temperature.
+	 * @param 	hotness
+	 * 			The new hotness to be set
+	 * @post	If the hotness is positive, the hotness is set to the given hotness;
+	 * 			if not, the hotness is set to zero.
+	 */
+	@Model
+	private void setHotness(long hotness) {
+		if (hotness > 0) {
+			this.hotness = hotness;
+		}
+	}
+
+	/**
+	 * A variable referencing the coldness of the temperature.
+	 */
+	private long coldness;
+
+	/**
+	 * A getter for the coldness of the temperature.
+	 */
+	@Basic
+	public long getColdness() {
+		return coldness;
+	}
+
+	@Model
+	private void setColdness(long coldness) {
+		if (coldness > 0) {
+			this.coldness = coldness;
+		}
+	}
 
 	protected void heat(long amount) {
 		if (amount > 0) {
@@ -37,12 +101,10 @@ public class Temperature {
 					hotness += amount;
 				}
 			}
-		} else {
-			throw new IllegalArgumentException("Temperature cannot be negative");
 		}
 	}
 
-	protected void cool(int amount) {
+	protected void cool(long amount) {
 		if (amount > 0) {
 			if (coldness == 0) {
 				hotness -= amount;
@@ -57,19 +119,14 @@ public class Temperature {
 					coldness += amount;
 				}
 			}
-		} else {
-			throw new IllegalArgumentException("Temperature cannot be negative");
 		}
 	}
 
-	public long getHotness() {
-		return hotness;
-	}
-
-	public long getColdness() {
-		return coldness;
-	}
-
+	/**
+	 * A method for getting both the hotness and the coldness of the temperature.
+	 * @return	An array containing the hotness and the coldness of the temperature.
+	 * 			| result == new long[]{getHotness(), getColdness()}
+	 */
 	public long[] getTemperature() {
 		return new long[] {hotness, coldness};
 	}
