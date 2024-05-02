21b69fd93bcf42c21d2d97d8cd26ccdcfe15d7c8
Flor De Meulemeester <flor.de.meulemeester@gmail.com>
2024-05-02 08:50:44 +0200

    Added some specification in Unit

--- src/rpg/Unit.java
+++ src/rpg/Unit.java
@@ -1,7 +1,16 @@
 package rpg;
 import be.kuleuven.cs.som.annotate.*;
-import rpg.State;
 
+/**
+ * An enumeration of units.
+ *
+ * @note 	We use NOMINAL PROGRAMMING for this class.
+ *
+ * @author 	Vincent Van Schependom
+ * @author	Arne Claerhout
+ * @author 	Flor De Meulemeester
+ * @version 1.0
+ */
 public enum Unit {
 
 	PINCH(1.0/6, new State[]{State.POWDER}),
@@ -17,19 +26,45 @@ public enum Unit {
 	CHEST(1260, new State[]{State.POWDER}),
 	STOREROOM(6300, new State[]{State.LIQUID, State.POWDER});
 
+	/**
+	 * Variable with the spoon equivalent value of that unit.
+	 */
 	private final double spoonEquivalent;
+
+	/**
+	 * A list of states containing the states which the unit can be used for.
+	 */
 	private final State[] allowedStates;
 
+	/**
+	 * Initialize a new unit with a given spoon equivalent and a list of allowed states.
+	 *
+	 * @param 	spoonEquivalent
+	 *			The spoon equivalent value of the new unit.
+	 * @param 	allowedStates
+	 *			The list of allowed states which the new unit can be used for.
+	 * @post	The spoon equivalent of the new unit is set to the given spoon equivalent.
+	 * 			| new.getSpoonEquivalent().equals(spoonEquivalent)
+	 * @post	The list of allowed states of the new unit is set to the given list of allowed states.
+	 * 			| new.getAllowedStates().equals(allowedStates)
+	 */
+	@Model
 	private Unit(double spoonEquivalent, State[] allowedStates) {
 		this.spoonEquivalent = spoonEquivalent;
 		this.allowedStates = allowedStates;
 	}
 
+	/**
+	 * Return the spoon equivalent of this unit.
+	 */
 	@Basic
 	public double getSpoonEquivalent() {
 		return spoonEquivalent;
 	}
 
+	/**
+	 * Return the list of allowed states of this unit.
+	 */
 	@Basic
 	public State[] getAllowedStates() {
 		return allowedStates;
@@ -46,6 +81,13 @@ public enum Unit {
 		return false;
 	}
 
+	/**
+	 * A method to convert the spoon equivalent value to a different unit.
+	 *
+	 * @param 	unit
+	 * 			The unit you want to convert to.
+	 * @return 	...
+	 */
 	public double getConversionFor(Unit unit) {
 		if (conversionAllowed(unit)) {
 			return this.spoonEquivalent / unit.spoonEquivalent;
