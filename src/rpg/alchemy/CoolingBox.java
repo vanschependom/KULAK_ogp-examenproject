7235356086c46d39c2c285c94627a04a72eedf42
Vincent Van Schependom <vincent.van.schependom@gmail.com>
2024-05-05 17:20:14 +0200

    Added specification of CoolingBox execute

--- src/rpg/alchemy/CoolingBox.java
+++ src/rpg/alchemy/CoolingBox.java
@@ -27,6 +27,12 @@ public class CoolingBox extends TemperatureDevice {
 	/**
 	 * A method that executes the operation of the cooling box device.
 	 *
+	 * @post	If the temperature of the cooling box is not hotter than the temperature of the ingredient,
+	 * 			the ingredient is cooled to the temperature of the cooling box.
+	 * 			| if !getTemperature().isHotterThan(getIngredientAt(0).getTemperature())
+	 * 			|	then getIngredientAt(0).getHotness() == getTemperature().getHotness() &&
+	 * 			|		 getIngredientAt(0).getColdness() == getTemperature().getHotness()
+	 *
 	 * @throws 	IllegalStateException
 	 * 			There are no items in the device
 	 * 			| isEmpty()
@@ -37,9 +43,7 @@ public class CoolingBox extends TemperatureDevice {
 			throw new IllegalStateException("There are no items in the device!");
 		}
 		// if the temperature of the cooling box is higher than the temperature of the ingredient, do nothing
-		if (getTemperature().isHotterThan(getIngredientAt(0).getTemperature())) {
-			return;
-		} else {
+		if (!getTemperature().isHotterThan(getIngredientAt(0).getTemperature())) {
 			// if the temperature of the ingredient is higher than the temperature of the cooling box, cool the ingredient
 			long difference = getTemperature().difference(getIngredientAt(0).getTemperature());
 			getIngredientAt(0).cool(difference);
