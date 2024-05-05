57f37600acc254992afd2b385e4f8592a9c72a29
Vincent Van Schependom <vincent.van.schependom@gmail.com>
2024-05-05 14:29:47 +0200

    Device

--- src/rpg/alchemy/Device.java
+++ src/rpg/alchemy/Device.java
@@ -20,16 +20,6 @@ import rpg.exceptions.DeviceNotYetUsedException;
  */
 public abstract class Device extends StorageLocation{
 
-    /**
-     * A variable containing the laboratory in which this device is stored.
-     *
-     * @note    This is done bi-directionally, all the main methods are
-     *          worked out in Laboratory
-     */
-    private Laboratory laboratory;
-
-    private boolean isTerminated = false;
-
     /**********************************************************
      * CONSTRUCTORS
      **********************************************************/
@@ -49,6 +39,12 @@ public abstract class Device extends StorageLocation{
         //TODO
     }
 
+
+
+    /**********************************************************
+     * RESULT
+     **********************************************************/
+
     /**
      * Return the result of the device in an ingredient container
      * with a maximum of Unit.getMaximumUnitForContainer(getState())
@@ -60,7 +56,21 @@ public abstract class Device extends StorageLocation{
      * @throws  IllegalStateException
      *          The device is terminated.
      *          | isTerminated()
-     * @return  TODO
+     *
+     * @return  If there are no ingredients in the device, return null
+     *          | if getNbOfIngredients() == 0
+     *          |   then result == null
+     * @return  If the spoon amount of the result is more than the spoon equivalent of the maximum
+     *          unit for a container with the state of the result, the excess goes to waste.
+     *          | if result.getSpoonAmount() > Unit.getMaxUnitForContainer(getIngredientAt(0).getState()).getSpoonEquivalent()
+     *          |   then result.equals(new AlchemicIngredient(1, Unit.getMaxUnitForContainer(getIngredientAt(0)), getIngredientAt(0).getTemperature(),
+     *          |                       getIngredientAt(0).getType(), getIngredientAt(0).getState()))
+     *          TODO write equals method for AlchemicIngredient
+     * @return  If the spoon amount of the result is not more than the spoon equivalent of the maximum
+     *          unit for a container with the state of the result, return a new container with the minimum unit
+     *          for the result, given the state and size of the result, containing the result.
+     *          | if result.getSpoonAmount() <= Unit.getMaxUnitForContainer(result.getState()).getSpoonEquivalent()
+     *          |   then result.equals(new IngredientContainer(Unit.getMinUnitForContainer(getIngredientAt(0)), getIngredientAt(0))
      */
     public IngredientContainer getResult() throws DeviceNotYetUsedException, IllegalStateException {
         if (isTerminated()) {
@@ -85,23 +95,27 @@ public abstract class Device extends StorageLocation{
         return new IngredientContainer(Unit.getMinUnitForContainer(result), result);
     }
 
+
+
+    /**********************************************************
+     * LABORATORY
+     **********************************************************/
+
     /**
-     * Return the laboratory in which this device is in.
+     * A variable containing the laboratory in which this device is stored.
      *
-     * @throws  IllegalStateException
-     *          The device is terminated.
-     *          | isTerminated()
+     * @note    This is done bi-directionally, all the main methods are
+     *          worked out in Laboratory
+     */
+    private Laboratory laboratory;
+
+    /**
+     * A method for getting the laboratory of the device.
      */
     @Basic
     public Laboratory getLaboratory() throws IllegalStateException {
-        if (isTerminated) throw new IllegalStateException("Device is terminated");
         return laboratory;
     }
-
-    /**
-     * A method for executing the device.
-     */
-    public void executeOperation() {}
 
     /**
      * A method for checking if a laboratory is valid for a device.
@@ -117,6 +131,25 @@ public abstract class Device extends StorageLocation{
         // return laboratory != null && laboratory.isValidDevice(this); TODO
         return false;
     }
+
+
+
+    /**********************************************************
+     * OPERATION EXECUTION
+     **********************************************************/
+
+    /**
+     * An abstract method for executing the device instructions.
+     */
+    public abstract void executeOperation();
+
+
+
+    /**********************************************************
+     * DESTRUCTION
+     **********************************************************/
+
+    private boolean isTerminated = false;
 
     /**
      * Return whether or not the device is terminated.
@@ -141,4 +174,5 @@ public abstract class Device extends StorageLocation{
         if (!isTerminated()) {isTerminated = true;}
         else throw new IllegalStateException("Device is terminated");
     }
+
 }
