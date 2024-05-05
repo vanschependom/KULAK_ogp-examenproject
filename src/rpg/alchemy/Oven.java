7b509b650fbb9ed47c1858b900e12ac27c738371
Arne Claerhout <arne31.claerhout@gmail.com>
2024-05-05 17:09:24 +0200

    Copied cooling box to oven

--- src/rpg/alchemy/Oven.java
+++ src/rpg/alchemy/Oven.java
@@ -2,8 +2,40 @@ package rpg.alchemy;
 
 public class Oven extends TemperatureDevice{
 
-	public Oven() {
-		super(null, null);
+	/**
+	 * A constructor for an oven.
+	 *
+	 * @param 	laboratory
+	 * 			The laboratory in which the oven will be situated.
+	 * @param 	temperature
+	 * 			The cooling temperature for the oven.
+	 *
+	 * @effect	A temperature device with given laboratory and temperature is created
+	 * 			| super(laboratory, temperature)
+	 */
+	public Oven(Laboratory laboratory, Temperature temperature) throws IllegalArgumentException {
+		super(laboratory, temperature);
+	}
+
+	/**
+	 * A method that executes the oven device
+	 *
+	 * @post	The ingredient in the device is heated to the temperature
+	 * 			of the device.
+	 * 			If the ingredient is already warmer, nothing happens
+	 * 			| if getTemperature().isHotterThan(getIngredientAt(0).getTemperatureObject())
+	 * 			| then getIngredientAt(0).heat(getIngredientAt(0).getTemperatureObject().difference(getTemperature()))
+	 * @throws 	IllegalStateException
+	 * 			There are no items in the device
+	 * 			| getNbOfIngredients() == 0
+	 */
+	@Override
+	public void executeOperation() throws IllegalStateException {
+		if (getNbOfIngredients() == 0) throw new IllegalStateException("No ingredients in Cooling Box");
+		AlchemicIngredient ing = getIngredientAt(0);
+		if (getTemperature().isHotterThan(ing.getTemperatureObject())) {
+			ing.heat(ing.getTemperatureObject().difference(getTemperature()));
+		}
 	}
 
 }
