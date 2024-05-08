3f655c33e7470ea73551af81d7b7f3f99b0c41f8
ArneCrash <arne31.claerhout@gmail.com>
2024-05-08 18:44:25 +0200

    Restructured Kettle

--- src/rpg/alchemy/Kettle.java
+++ src/rpg/alchemy/Kettle.java
@@ -34,67 +34,124 @@ public class Kettle extends Device {
 	}
 
 	/**
+	 * Return the new name object for the new mixed ingredient, with a special name of null.
+	 * The simple name parts being all the simple name parts of the ingredients.
+	 */
+	@Model @Basic
+	private Name getNewName() {
+		ArrayList<String> names = new ArrayList<>();
+
+		for (int i = 0; i < getNbOfIngredients(); i++) {
+			// we get all names of the ingredients
+			names.addAll(Arrays.asList(getIngredientAt(i).getType().getName().getSimpleNameParts()));
+		}
+		// we return a new name with all the names of the ingredients
+		return new Name(null, String.valueOf(List.of(names)));
+	}
+
+	/**
+	 * Return the state of the ingredient with a standard temperature closest to [0, 20].
+	 * Liquid is chosen over powder in an ex aequo.
+	 */ // TODO kan  dit hier basic genomen worden of moet hier heel wat specificatie bijkomen met meer uitleg
+	// TODO geldt eigenlijk voor elke new method
+	@Model @Basic
+	private State getNewState() {
+		int closestTemperatureIndex = 0;
+		long smallestDifferenceTemperature = Long.MAX_VALUE;
+
+		for (int i = 0; i < getNbOfIngredients(); i++) {
+			// We get the ingredient with a standard temperature closest to [0, 20]
+			long difference = getIngredientAt(i).getType().getStandardTemperatureObject().difference(new long[]{0, 20})
+			if (difference < smallestDifferenceTemperature) {
+				smallestDifferenceTemperature = difference;
+				closestTemperatureIndex = i;
+			} else if (difference == smallestDifferenceTemperature && getIngredientAt(i).getState() == State.LIQUID) {
+				closestTemperatureIndex = i;
+			}
+		}
+		return getIngredientAt(closestTemperatureIndex).getState();
+	}
+
+	/**
+	 * Return the sum of all the spoon amounts of the ingredients
+	 */
+	@Model @Basic
+	private double getNewSpoonAmount() {
+		double totalSpoonAmount = 0;
+		for (int i = 0; i < getNbOfIngredients(); i++) {
+			// We add the amount to	the total spoon amount
+			totalSpoonAmount += getIngredientAt(i).getSpoonAmount();
+		}
+		return totalSpoonAmount;
+	}
+
+	/**
+	 * Return The standard temperature which is closest to [0, 20],
+	 * if the difference is the same, the hottest standard temperature is chosen
+	 */
+	@Model
+	private Temperature getNewStandardTemperature() {
+		Temperature smallestDifferenceTemperature = new Temperature(0, Temperature.getUpperbound());
+		long smallestDifference = smallestDifferenceTemperature.difference(new long[]{0, 20});
+		for (int i = 0; i < getNbOfIngredients(); i++) {
+			long difference = getIngredientAt(i).getType().getStandardTemperatureObject().difference(new long[]{0, 20});
+			// We get the ingredient with a standard temperature closest to [0, 20]
+			if ( difference < smallestDifference || ( difference == smallestDifference &&
+					getIngredientAt(i).getHotness() > smallestDifferenceTemperature.getHotness() ) ) {
+				smallestDifferenceTemperature = getIngredientAt(i).getType().getStandardTemperatureObject();
+				smallestDifference = smallestDifferenceTemperature.difference(new long[]{0, 20});
+			}
+		}
+		return smallestDifferenceTemperature;
+	}
+
+	/**
+	 * Return the weighted average temperature of all the ingredients inside the kettle.
+	 */
+	@Model @Basic
+	private Temperature getNewTemperature() {
+		Temperature totalTemperature = new Temperature(0, 0);
+		for (int i = 0; i < getNbOfIngredients(); i++) {
+			Temperature.add(getIngredientAt(i).getTemperatureObject(), totalTemperature).mul(getIngredientAt(i).getSpoonAmount());
+		}
+
+		totalTemperature.mul(1 / getNewSpoonAmount());
+		return totalTemperature;
+	}
+
+	/**
 	 * A method for executing a kettle
 	 *
 	 * @effect  Executes the operation from device
 	 *          | super.executeOperation()
+	 *
 	 * @post	All ingredients are deleted and a new ingredient is created with
-	 * 			a new combined name, a weighted average temperature,
-	 * 			the new state is the state of the ingredient with the standard temperature
-	 * 			closest to [0,20], the new temperature is the temperature of the ingredient
-	 * 			with the standard temperature closest to [0,20] and the new amount is the total
-	 * 			amount of all ingredients.
-	 * 			TODO
+	 * 			a new combined name, a weighted average temperature, a new state, a new standard temperature
+	 * 			and a new amount
+	 * 			| ( addAsIngredient(new AlchemicIngredient((int) newSpoonAmount, Unit.SPOON, newTemperature,
+	 * 			|	new IngredientType(newName, newState, newStandardTemperature, true), newState)) ) &&
+	 * 			| ( for each I in 0..getNbOfIngredients()-2:
+	 * 			|	removeAsIngredient(getIngredientAt(0)) )
 	 */
 	@Override
 	public void executeOperation() {
 		super.executeOperation();
 
-		if (getNbOfIngredients() == 1) {	// if only 1 ingredient, don't mix
+		// Get all the new values
+		Name newName = getNewName();
+		State newState = getNewState();
+		double newSpoonAmount = getNewSpoonAmount();
+		Temperature newStandardTemperature = getNewStandardTemperature();
+		Temperature newTemperature = getNewTemperature();
 
-			ArrayList<String> names = new ArrayList<>();
-			int closestTemperatureIndex = 0;
-			long smallestDifferenceTemperature = Long.MAX_VALUE;
-			double totalSpoonAmount = 0;
-			Temperature totalTemperature = new Temperature(0, 0);
-
-			for (int i = 0; i < getNbOfIngredients(); i++) {
-				// We get all the names of the ingredients
-				names.addAll(Arrays.asList(getIngredientAt(i).getType().getName().getSimpleNameParts()));
-				// We get the ingredient with a standard temperature closest to [0, 20]
-				if (getIngredientAt(i).getType().getStandardTemperatureObject().difference(new long[]{0, 20}) < smallestDifferenceTemperature) {
-					smallestDifferenceTemperature = getIngredientAt(i).getType().getStandardTemperatureObject().difference(new long[]{0, 20});
-					closestTemperatureIndex = i;
-				}
-				// We add the amount to	the total spoon amount
-				totalSpoonAmount += getIngredientAt(i).getSpoonAmount();
-				// We calculate the total temperature
-				Temperature.add(getIngredientAt(i).getTemperatureObject(), totalTemperature).mul(getIngredientAt(i).getSpoonAmount());
-			}
-
-			// Set the new name
-			Name newName = new Name(null, String.valueOf(List.of(names)));
-
-			// Set the new state
-			State newState = getIngredientAt(closestTemperatureIndex).getState();
-
-			// Set the newStandardTemperature
-			Temperature newStandardTemperature = getIngredientAt(closestTemperatureIndex).getType().getStandardTemperatureObject();
-
-			// Now divide with the total spoon amount to get the average temperature
-			totalTemperature.mul(1 / totalSpoonAmount);
-			Temperature averageTemperature = totalTemperature;
-
-			// delete all ingredients
-			while (getNbOfIngredients() > 0) {
-				removeAsIngredient(getIngredientAt(0));
-			}
-
-			// create the new ingredient(type) and add to the kettle
-			IngredientType newType = new IngredientType(newName, newState, newStandardTemperature, true);
-			addAsIngredient(new AlchemicIngredient((int) totalSpoonAmount, Unit.SPOON, averageTemperature, newType, newState));
-
+		// delete all ingredients
+		while (getNbOfIngredients() > 0) {
+			removeAsIngredient(getIngredientAt(0));
 		}
+
+		// create the new ingredient(type) and add to the kettle
+		IngredientType newType = new IngredientType(newName, newState, newStandardTemperature, true);
+		addAsIngredient(new AlchemicIngredient((int) newSpoonAmount, Unit.SPOON, newTemperature, newType, newState));
 	}
 
 }
